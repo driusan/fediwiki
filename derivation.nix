9f8b9280e1d64d8d6ba5e7f45845fed19b36f8e4
Dave MacFarlane <driusan@mcin.ca>
2022-12-24 13:30:31 -0500

    Fix nix-build

--- derivation.nix
+++ derivation.nix
@@ -6,7 +6,5 @@ buildGoModule rec {
         src = ./.;
 
         # vendorSha256 = "0000000000000000000000000000000000000000000000000000000000000000";
-        # vendorSha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        # vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-        vendorSha256 = "sha256-3fRHvy6YCUUJE+6iSOMFSqoAkflbWz/axIDAFVv6w7k=";
+        vendorSha256 = "sha256-bDihSeXuWVa311tvOqfCpBxYevJCAWaZd+m7GQeA6i0=";
 }
